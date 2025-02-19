4552f4caf44f0daa656b6198ade32eb044d567b5
Keith Lee <keithmlee19@gmail.com>
2025-02-19 18:47:00 -0500

    Updated Setup_TripleTriadGame.bat

--- Setup_TripleTriadGame.bat
+++ Setup_TripleTriadGame.bat
@@ -1,6 +1,10 @@
 @echo off
 echo Starting Pixel Streaming Server...
 
+:: Change to current directory
+echo Changing to current directory...
+cd /d "%~dp0"
+
 :: Get the current directory dynamically
 set "GAME_DIR=%~dp0"
 
@@ -10,10 +14,19 @@ set "PS_SERVER_DIR=%GAME_DIR%Windows\TripleTriad\Samples\PixelStreaming2\WebServ
 :: Define the game executable location (inside Windows folder)
 set "GAME_EXE=%GAME_DIR%Windows\TripleTriad.exe"
 
-:: Start the Pixel Streaming Server in a new window
+:: Setup the Pixel Streaming Server in a new window
 if exist "%PS_SERVER_DIR%\get_ps_servers.bat" (
-    echo Found Pixel Streaming Server. Starting...
+    echo Found get_ps_servers. Starting...
     start "PixelStreamingServer" cmd /c "%PS_SERVER_DIR%\get_ps_servers.bat"
+	:: Start the signalling server
+	if exist "%PS_SERVER_DIR%\SignallingWebServer\platform_scripts\cmd\start_with_stun.bat" (
+		echo Found start_with_stun. Starting...
+		start "Stun Script" cmd /c "%PS_SERVER_DIR%\SignallingWebServer\platform_scripts\cmd\start_with_stun.bat"
+	) else (
+		echo ERROR: Could not find start_with_stun.bat in %PS_SERVER_DIR%
+		pause
+		exit /b
+	)
 ) else (
     echo ERROR: Could not find get_ps_servers.bat in %PS_SERVER_DIR%
     pause
@@ -26,7 +39,7 @@ timeout /t 10 /nobreak
 :: Start the Triple Triad game with Pixel Streaming in a new window
 if exist "%GAME_EXE%" (
     echo Starting Triple Triad Game with Pixel Streaming...
-    start "TripleTriadGame" cmd /c "%GAME_EXE%" -PixelStreamingIP=127.0.0.1 -PixelStreamingPort=8888
+    start "TripleTriadGame" cmd /c "%GAME_EXE%" -PixelStreamingURL=ws://127.0.0.1:8888 -RenderOffScreen
 ) else (
     echo ERROR: Could not find TripleTriad.exe in %GAME_DIR%Windows\
     pause
