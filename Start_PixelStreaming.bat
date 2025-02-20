2e08b95b70a66b4c0e71bab2b9450d7b517c68ad
Jon Gracias <74846399+JonGracias@users.noreply.github.com>
2025-02-20 14:44:46 -0500

    Added Setup batch files and start batch files

--- Start_PixelStreaming.bat
+++ Start_PixelStreaming.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Starting Pixel Streaming Server Setup...
+echo Starting Pixel Streaming and Triple Triadzzz...
 
 :: Get the current directory dynamically
 set "GAME_DIR=%~dp0"
@@ -11,25 +11,6 @@ set "PS_CMD_DIR=%PS_SERVER_DIR%\SignallingWebServer\platform_scripts\cmd"
 :: Define the game executable location (inside Windows folder)
 set "GAME_EXE=%GAME_DIR%Windows\TripleTriad.exe"
 
-:: Build Pixel Streaming Infrastructure
-if exist "%PS_SERVER_DIR%\get_ps_servers.bat" (
-    echo Found Pixel Streaming Server. Starting setup...
-    start /wait "PixelStreamingServer" cmd /c "%PS_SERVER_DIR%\get_ps_servers.bat"
-) else (
-    echo ERROR: Could not find get_ps_servers.bat in %PS_SERVER_DIR%
-    pause
-    exit /b 1
-)
-
-:: Run the Pixel Streaming setup script
-if exist "%PS_CMD_DIR%\setup.bat" (
-    echo Running Pixel Streaming setup...
-    start /wait "SetupPixelStreaming" cmd /c "%PS_CMD_DIR%\setup.bat"
-) else (
-    echo ERROR: Could not find setup.bat in %PS_CMD_DIR%
-    pause
-    exit /b 1
-)
 
 :: Start Pixel Streaming with STUN
 if exist "%PS_CMD_DIR%\start_with_stun.bat" (
